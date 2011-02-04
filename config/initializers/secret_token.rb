70102b1eda63fc7494a877d6279f36fa9a31e432
Andrea <andrea.damico@gmail.com>
2011-02-04 19:57:37 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Informataide::Application.config.secret_token = '70d08f6659657e7f97ee81c78b09b1d5092f9f69addcfbc385f1fe939a720b10701a412a6331344ab91a68fcc93056e4382a6c0e2df17785fdb3ea5567f74723'
